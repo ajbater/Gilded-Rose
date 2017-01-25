05c0704dc34b754c1f82295c3e16c4885fd470a4
ajbater <amanda22@hotmail.co.uk>
2017-01-25 13:36:58 +0000

    removes conditional statement from #update_quality - three tests still passing

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -45,55 +45,68 @@ class GildedRose
     decrease_sell_in_value(item)
   end
 
-  
+  def update_backstage_passes(item)
+    if item.sell_in <= 5 && item.quality < MAXIMUM_QUALITY
+      3.times { decrease_quality(item) }
+    elsif item.sell_in <= 10 && item.quality < MAXIMUM_QUALITY
+      2.times { decrease_quality(item) }
+    else
+      decrease_quality(item)
+    end
+    decrease_sell_in_value(item)
+  end
 
   def update_quality()
-    @items.each do |item|
-      if normal_item?(item)
-        if item.quality > MINIMUM_QUALITY
-          if !sulfuras?(item)
-            item.quality = item.quality - 1
-          end
-        end
-      else
-        if item.quality < MAXIMUM_QUALITY
-          item.quality = item.quality + 1
-          if backstage_passes?(item)
-            if item.sell_in < 11
-              if item.quality < MAXIMUM_QUALITY
-                item.quality = item.quality + 1
-              end
-            end
-            if item.sell_in < 6
-              if item.quality < MAXIMUM_QUALITY
-                item.quality = item.quality + 1
-              end
-            end
-          end
-        end
-      end
-      if !sulfuras?(item)
-        decrease_sell_in_value(item)
-      end
-      if item.sell_in < 0
-        if !aged_brie?(item)
-          if !backstage_passes?(item)
-            if item.quality > MINIMUM_QUALITY
-              if !sulfuras?(item)
-                item.quality = item.quality - 1
-              end
-            end
-          else
-            item.quality = item.quality - item.quality
-          end
-        else
-          if item.quality < MAXIMUM_QUALITY
-            item.quality = item.quality + 1
-          end
-        end
-      end
-    end
+
   end
+
+#   def update_quality()
+#     @items.each do |item|
+#       if normal_item?(item)
+#         if item.quality > MINIMUM_QUALITY
+#           if !sulfuras?(item)
+#             item.quality = item.quality - 1
+#           end
+#         end
+#       else
+#         if item.quality < MAXIMUM_QUALITY
+#           item.quality = item.quality + 1
+#           if backstage_passes?(item)
+#             if item.sell_in < 11
+#               if item.quality < MAXIMUM_QUALITY
+#                 item.quality = item.quality + 1
+#               end
+#             end
+#             if item.sell_in < 6
+#               if item.quality < MAXIMUM_QUALITY
+#                 item.quality = item.quality + 1
+#               end
+#             end
+#           end
+#         end
+#       end
+#       if !sulfuras?(item)
+#         decrease_sell_in_value(item)
+#       end
+#       if item.sell_in < 0
+#         if !aged_brie?(item)
+#           if !backstage_passes?(item)
+#             if item.quality > MINIMUM_QUALITY
+#               if !sulfuras?(item)
+#                 item.quality = item.quality - 1
+#               end
+#             end
+#           else
+#             item.quality = item.quality - item.quality
+#           end
+#         else
+#           if item.quality < MAXIMUM_QUALITY
+#             item.quality = item.quality + 1
+#           end
+#         end
+#       end
+#     end
+#   end
 end
 
 class Item
