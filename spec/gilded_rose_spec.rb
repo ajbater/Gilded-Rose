763ae58eea4a20d71794f0b344f1d99811329f26
ajbater <amanda22@hotmail.co.uk>
2017-01-25 10:46:54 +0000

    tests that #update_quality increases the quality of brie

--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -6,32 +6,41 @@ describe GildedRose do
 
   describe "#update_quality" do
     it "does not change the name" do
-      items = [Item.new("foo", 0, 0)]
+      items = [Item.new("Normal Item", 0, 0)]
       GildedRose.new(items).update_quality()
-      expect(items[0].name).to eq "foo"
+      expect(items[0].name).to eq "Normal Item"
     end
 
     context "for a normal item" do
       it "if the sell_in value has not yet been reached, it lowers the quality by 1" do
-        items = [Item.new("normal_item", 5, 10)]
+        items = [Item.new("Normal Item", 5, 10)]
         gilded_rose = GildedRose.new(items)
         gilded_rose.update_quality()
         expect(items[0].quality).to eq 9
       end
 
       it "decreases the sell_in value by 1" do
-        items = [Item.new("normal_item", 5, 10)]
+        items = [Item.new("Normal Item", 5, 10)]
         gilded_rose = GildedRose.new(items)
         gilded_rose.update_quality()
         expect(items[0].sell_in).to eq 4
       end
 
       it "decreases the quality by 2 after the sell_in value has been reached" do
-        items = [Item.new("normal_item", 5, 10)]
+        items = [Item.new("Normal Item", 5, 10)]
         gilded_rose = GildedRose.new(items)
         5.times { gilded_rose.update_quality() }
         gilded_rose.update_quality
         expect(expect(items[0].quality).to eq 3)
+      end
+    end
+
+    context "for Aged Brie" do
+      it "if the item is Aged Brie, the quality will increase as the sell_in date approaches" do
+        items = [Item.new("Aged Brie", 5, 10)]
+        gilded_rose = GildedRose.new(items)
+        gilded_rose.update_quality()
+        expect(items[0].quality).to eq 11
       end
     end
   end
@@ -40,7 +49,7 @@ end
 
 describe Item do
   it "the quality of an item can never go below 0" do
-    items = [Item.new("normal_item", 5, 5)]
+    items = [Item.new("Normal Item", 5, 5)]
     gilded_rose = GildedRose.new(items)
     6.times { gilded_rose.update_quality() }
     expect(items[0].quality).to eq 0
