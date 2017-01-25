0aac1b23530c02ce955a8cea3bfc6323e2ca0735
ajbater <amanda22@hotmail.co.uk>
2017-01-25 11:13:13 +0000

    tests that #update_quality increases the quality of backstage passes at a higher rate if the sell_in value is 5 or less

--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -81,6 +81,13 @@ describe GildedRose do
         gilded_rose.update_quality()
         expect(items[0].quality).to eq 3
       end
+
+      it "increases the quality by 3 when there are 5 days or left until sell_in value" do
+        items = [Item.new("Backstage passes to a TAFKAL80ETC concert", 5, 1)]
+        gilded_rose = GildedRose.new(items)
+        gilded_rose.update_quality()
+        expect(items[0].quality).to eq 4
+      end
     end
   end
 
