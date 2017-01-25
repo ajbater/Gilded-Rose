e307cc2c21f0b64380ec2388ae30c244631e599a
ajbater <amanda22@hotmail.co.uk>
2017-01-25 10:32:04 +0000

    tests that #update_quality reduces normal item quality by 1

--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -10,6 +10,15 @@ describe GildedRose do
       GildedRose.new(items).update_quality()
       expect(items[0].name).to eq "foo"
     end
+
+    context "for a normal item" do
+      it "if the sell_in value has not yet been reached, it lowers the quality by 1" do
+        items = [Item.new("normal_item", 5, 5)]
+        gilded_rose = GildedRose.new(items)
+        gilded_rose.update_quality()
+        expect(items[0].quality).to eq 4
+      end
+    end
   end
 
 end
