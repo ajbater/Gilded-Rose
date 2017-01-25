e620de4e4ac24417979dae21377cc0fa763637c0
ajbater <amanda22@hotmail.co.uk>
2017-01-25 10:43:43 +0000

    tests that the quality of an item can never go below 0

--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -37,3 +37,12 @@ describe GildedRose do
   end
 
 end
+
+describe Item do
+  it "the quality of an item can never go below 0" do
+    items = [Item.new("normal_item", 5, 5)]
+    gilded_rose = GildedRose.new(items)
+    6.times { gilded_rose.update_quality() }
+    expect(items[0].quality).to eq 0
+  end
+end
